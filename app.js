03dcb7ac7531d038e67b9be1a63369889a9c1dd5
Thyago Volpatto <tvolpatto@gmail.com>
2020-02-19 12:07:16 -0700

    function to add employees

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var table = require("console.table");
 var db = require("./lib/database");
 const Department = require("./lib/department");
 const Role = require("./lib/role");
+const Employee = require("./lib/employee");
 
 function start() {
   db.open().then(() => {
@@ -46,7 +47,7 @@ function getQuestions() {
     {
       key: 6,
       name: "Add a new employee.",
-      value: "addRole"
+      value: "addEmployee"
     },
     {
       key: 7,
@@ -76,6 +77,7 @@ function setNextAction(action) {
       viewDepartments();
       break;
     case "addEmployee":
+      addEmployee()
       break;
     case "addRole":
       addRole();
@@ -83,7 +85,7 @@ function setNextAction(action) {
     case "addDepartment":
       addDepartment();
       break;
-    case "exit" :
+    case "exit":
       return db.close();
 
   }
@@ -91,7 +93,7 @@ function setNextAction(action) {
 
 function viewEmployees() {
   const query = "select e.id, e.first_name, e.last_name, r.title, d.name, r.salary, CONCAT(m.first_name,' ', m.last_name) as manager from employee e" +
-  " left join role r on r.id=e.role_id left join department d on r.department_id=d.id left join employee m on m.id=e.manager_id";
+    " left join role r on r.id=e.role_id left join department d on r.department_id=d.id left join employee m on m.id=e.manager_id";
   db.selectAllFrom(query).then((res, err) => {
     if (err) throw err;
     console.table(res);
@@ -126,7 +128,7 @@ function addDepartment() {
     message: "What is the name of the new department?",
     name: "name"
   };
-  inquirer.prompt(question).then((answer) =>{
+  inquirer.prompt(question).then((answer) => {
     var department = new Department(null, answer.name);
     db.addNewRegistry("insert into department set ?", department).then((res) => {
       console.log(res);
@@ -136,18 +138,19 @@ function addDepartment() {
 }
 
 function addRole() {
-  db.selectAllFrom("select * from department order by name").then(departments =>{
+  db.selectAllFrom("select * from department order by name").then(departments => {
     const choices = []
-    departments.forEach(d =>{
-      choices.push ({
+    departments.forEach(d => {
+      choices.push({
         name: d.name,
-        value: d.id      });
+        value: d.id
+      });
     });
 
-    const questions =[
+    const questions = [
       {
         type: "input",
-        message: "What is the title of the new department?",
+        message: "What is the title of the new role?",
         name: "title"
       },
       {
@@ -159,25 +162,72 @@ function addRole() {
         type: "list",
         message: "Select the new role department:",
         name: "department",
-        choices : choices
+        choices: choices
       }];
 
-      inquirer.prompt(questions).then(answers => {
-        const role = new Role(null, answers.title, answers.salary, answers.department);
+    inquirer.prompt(questions).then(answers => {
+      const role = new Role(null, answers.title, answers.salary, answers.department);
 
-        db.addNewRegistry("insert into role set ?", role).then((res) => {
-          console.log(res);
-          viewRoles();
-        });
-      })
+      db.addNewRegistry("insert into role set ?", role).then((res) => {
+        console.log(res);
+        viewRoles();
+      });
+    })
   });
-  
+
 }
 
-function addEmployee(employee) {
-  db.addNewRegistry("insert into employee set ?", employee).then((res) => {
-    console.log(res);
+function addEmployee() {
+  db.selectAllFrom("select id as value, title as name from role order by title").then(roles => {
+    db.selectAllFrom("select id as value, CONCAT(first_name,' ', last_name) as name from employee order by name").then(managers => {
+
+      const questions = [
+        {
+          type: "input",
+          message: "What is the employee first name?",
+          name: "first_name"
+        },
+        {
+          type: "input",
+          message: "What is the employee last name?",
+          name: "last_name"
+        },
+        {
+          type: "list",
+          message: "Select the new employee's role:",
+          name: "role",
+          choices: roles
+        },
+        {
+          type: "confirm",
+          message: "Do you want to set up a manager?",
+          name: "need_manager",
+        },
+        {
+          type: "list",
+          message: "Select the new employee's manager:",
+          name: "manager",
+          choices: managers,
+          when: function (answer) {
+            if (!answer.need_manager) {
+              return null;
+            } else {
+              return this;
+            }
+          }
+        }];
+
+      inquirer.prompt(questions).then(answers => {
+        const employee = new Employee(null, answers.first_name, answers.last_name, answers.role, answers.manager);
+    
+        db.addNewRegistry("insert into employee set ?", employee).then((res) => {
+          console.log(res);
+          viewEmployees();
+         });
+      });
+    });
   });
+
 }
 
 
