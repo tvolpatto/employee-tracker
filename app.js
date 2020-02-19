c0fff2ccaa52e998d8915282cc72ce3e6b07a341
Thyago Volpatto <tvolpatto@gmail.com>
2020-02-19 09:08:43 -0700

    moved database access functuins to another file

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-var mysql = require("mysql");
 var inquirer = require("inquirer");
 var table = require("console.table");
+var db = require("./lib/database");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "password",
-  database: "company_db"
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  getAllEmployees();
-});
-
-function getAllEmployees() {
-    let query = "select e.id, e.first_name, e.last_name, r.title, d.name, r.salary, CONCAT(m.first_name,' ', m.last_name) as manager from employee e"+ 
-        " left join role r on r.id=e.role_id left join department d on r.department_id=d.id left join employee m on m.id=e.manager_id";
-    connection.query(query, function(err, res) {
-        if (err) throw err;
-        console.table(res);
-        connection.end();
+function start() {
+  db.open().then(() => {
+    db.getAllEmployees().then((res, err)=>{
+      if(err) throw err;
+      console.table(res);
+      db.close();
     });
+
+  });
+}
+
+start();
+
+
+
+function addDepartment(department) {
+  db.addNewRegistry("insert into department set ?", department).then((res) =>{
+    console.log(res);
+  });
+}
+
+function addRole(role) {
+  db.addNewRegistry("insert into role set ?", role).then((res) =>{
+    console.log(res);
+  });
+}
+
+function addEmployee(employee) {
+  db.addNewRegistry("insert into employee set ?", employee).then((res) =>{
+    console.log(res);
+  });
 }
 
