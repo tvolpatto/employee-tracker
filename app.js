cd7fc6c45da5fd8782266fc198b375c532ae3d63
Thyago Volpatto <tvolpatto@gmail.com>
2020-02-18 21:20:07 -0700

    created query to select all employees

--- app.js
+++ app.js
@@ -19,4 +19,15 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  getAllEmployees();
 });
+
+function getAllEmployees() {
+    let query = "select e.id, e.first_name, e.last_name, r.title, d.name, r.salary, CONCAT(m.first_name,' ', m.last_name) as man_name from employee e"+ 
+        " left join role r on r.id=e.role_id left join department d on r.department_id=d.id left join employee m on m.id=e.manager_id";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+}
